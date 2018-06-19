c9af75dcc38566e3485dc9da235248d7f6f280a8
void <ima10088862165vs@163.com>
2018-06-19 12:17:42 +0800

    Update payroll.sol

--- projects/lesson-1/contracts/payroll.sol
+++ projects/lesson-1/contracts/payroll.sol
@@ -32,7 +32,7 @@ contract Payroll {
         curAddr = newAddr;
     }
     
-    function getBalance() payable public returns(uint){
+    function addFund() payable public returns(uint){
         return this.balance;
     }
     
